e551acd048e3e54763ea187d2e634f97867e9e22
abbyammo13@gmail.com <abbyammo13@gmail.com>
2025-03-05 23:26:01 +0000

    changing api urls for frontend

--- backend/core/models.py
+++ backend/core/models.py
@@ -38,6 +38,7 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True, blank=True, related_name="users")
+    is_current_school_member= models.BooleanField(default=True)
     
     def __str__(self):
         return self.username
