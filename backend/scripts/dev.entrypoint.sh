0b67272109951ef0935a3795bf7cbf826d40b28b
abbyammo13@gmail.com <abbyammo13@gmail.com>
2025-03-22 20:41:22 +0000

    wip: fixing and improving docker files

--- backend/scripts/dev.entrypoint.sh
+++ backend/scripts/dev.entrypoint.sh
@@ -5,7 +5,7 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $AUTHENTICATION_DB_HOST $DB_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
 
     echo "PostgreSQL started"
@@ -16,20 +16,13 @@ then
     #python manage.py runserver 0.0.0.0:8000
     if [ "$DEBUG" = "True" ]
     then
-      python manage.py runserver 0.0.0.0:8000
+        python manage.py runserver 0.0.0.0:8000;
     else
-      gunicorn backend.wsgi:application --bind 0.0.0.0:8000
-      
+        gunicorn backend.wsgi:application --bind 0.0.0.0:8000;
     fi
-
 
     if [ "$DJANGO_SUPERUSER_USERNAME" ]
     then
-        python manage.py createsuperuser \
-            --noinput \
+        python manage.py createsuperuser --noinput;
     fi
-
 fi
-
-
-exec "$@"
