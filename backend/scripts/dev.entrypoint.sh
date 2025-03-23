a4b1283d82ec8963d521e01bedfc1ff020d79ec7
Godly Obeng <me@godlyobeng.com>
2025-03-23 11:28:02 +0000

    attempt to fix entrypoint

--- backend/scripts/dev.entrypoint.sh
+++ backend/scripts/dev.entrypoint.sh
@@ -1,25 +1,23 @@
-#!/bin/sh
+#!/bin/ash
 
 if [ "$DATABASE" = "postgres" ]
-then
     echo "Waiting for postgres..."
-
     while ! nc -z $AUTHENTICATION_DB_HOST $DB_PORT; do
-        sleep 0.1
+        sleep 1
     done
 
     echo "PostgreSQL started"
     python manage.py makemigrations
     python manage.py migrate
 
+    echo "Running collectstatic"
     python manage.py collectstatic --noinput
-    #python manage.py runserver 0.0.0.0:8000
     if [ "$DEBUG" = "True" ]
-    then
         python manage.py runserver 0.0.0.0:8000;
     else
         gunicorn backend.wsgi:application --bind 0.0.0.0:8000;
     fi
 
+    echo "Create super user"
     python manage.py createsuperuser --noinput;
 fi
