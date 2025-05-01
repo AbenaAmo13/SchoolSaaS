b02d5380eead44c1fe64425c3ab97e8113e422e4
abbyammo13@gmail.com <abbyammo13@gmail.com>
2025-05-01 19:52:44 +0000

    fixing errors

--- frontend/src/Components/ApplicationNavigation.jsx
+++ frontend/src/Components/ApplicationNavigation.jsx
@@ -1,27 +1,15 @@
 import React, { useEffect } from 'react';
 import { Outlet} from "react-router";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 
 
 const ApplicationNavigation = ({ children }) => {
   
   return (
     <div className='container'>
-        <nav>
-          <Link
-            to={{
-              pathname: "/courses",
-              search: "?sort=name",
-              hash: "#the-hash",
-              state: { fromDashboard: true }
-            }}
-          />
-        </nav>
+        <h1>
+          Homepage
+        </h1>
         <main>
         <Outlet />; {/* This is where the specific pages like Login or Register will be rendered */}
         </main>
